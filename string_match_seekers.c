b0f4f2d151e626774559dffe043700fe33aeb8fb
suyash <suyash2796@gmail.com>
2017-08-09 00:34:06 +0530

    new array initialized

--- string_match_seekers.c
+++ string_match_seekers.c
@@ -1,11 +1,26 @@
+/*
+C program to find the numbers of times a string 'seekers'
+can be formed from a given string
+*/
+
 #include<stdio.h>
-#define size 100
+
 
 int main()
 {
+  /*
+  char array initialized
+  */
   char arr[]="ssdjadwfuweifeekrssejbfjlkrrwgrssekktaserskk";
   int i,count_s=0,count_e=0,count_k=0,count_r=0;
   int k[4],min;
+
+  //loop starts from here to match chars of string
+  /*
+  loop will count the number of time characters of string "seekers"
+  i.e s, e,k,r appear in the given string. Count of s,e,k,r will
+  be stored in a new array say k.
+  */
   for(i=0; i < sizeof(arr);i++)
   {
     if(arr[i]=='s')
@@ -37,7 +52,8 @@ int main()
            continue;
          }
 
-      min=k[0];
+      min=k[0]; // min initialized to first element of k
+
       for ( i = 1; i < 4; i++)
        {
         if(min>k[i])
