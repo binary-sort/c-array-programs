2afbce47eddd45961d13059d34b52350e89b5801
suyash <suyash2796@gmail.com>
2017-08-08 22:54:08 +0530

    comments added

--- max_pair_gcd.c
+++ max_pair_gcd.c
@@ -1,10 +1,19 @@
+/*
+C program to find pair with maximum GCD in an integer array
+*/
 #include <stdio.h>
-#define size 10
+
+#define size 10  //size of array defined
+
 int gcd(int n1, int n2);
+/*
+main function starts from here
+*/
 int main()
 {
-  int arr[size]={2,4,6,9,3,16,12,45,8,76},x,p1=0,p2=0;
+  int arr[size]={2,4,6,9,3,16,12,45,8,76},x,p1=0,p2=0;//array initailzed
   x=gcd(arr[0], arr[1]);
+//loop starts from here to find pair with maximum GCD
   for(int i = 0; i < 10; i++) {
     for(int j = i + 2; j < 10; j++) {
 
