8573697bbeb7658eaeb1e78945a14f82882df06d
suyash <suyash2796@gmail.com>
2017-08-09 00:24:38 +0530

    string match problem initailized

---
+++ string_match_seekers.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#define size 100
+
+int main()
+{
+  char arr[]="ssdjadwfuweifeekrssejbfjlkrrwgrssekktaserskk";
+  int i,count_s=0,count_e=0,count_k=0,count_r=0;
+  int k[4],min;
+  for(i=0; i < sizeof(arr);i++)
+  {
+    if(arr[i]=='s')
+    {
+      count_s=count_s+1;
+      k[0]=count_s;
+    }
+    else
+      if(arr[i]=='e')
+      {
+        count_e=count_e+1;
+        k[1]=count_e;
+      }
+      else
+        if(arr[i]=='k')
+        {
+          count_k=count_k+1;
+          k[2]=count_k;
+        }
+
+        else
+          if(arr[i]=='r')
+          {
+            count_r=count_r+1;
+            k[3]=count_r;
+          }
+
+        else
+           continue;
+         }
+
+      min=k[0];
+      for ( i = 1; i < 4; i++)
+       {
+        if(min>k[i])
+        {
+          min=k[i];
+        }
+
+      }
+      printf("%d", min );
+      printf("\n" );
+
+      return 0;
+
+
+}
