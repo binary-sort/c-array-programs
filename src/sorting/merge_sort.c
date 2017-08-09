8fd65e1a2999d44cd73d105caa5f0d8b14eb7068
Mayank <mayankdubey291@gmail.com>
2017-08-10 00:44:25 +0530

    Minor

--- src/sorting/merge_sort.c
+++ src/sorting/merge_sort.c
@@ -5,30 +5,30 @@ void merge(int array[], int l, int m, int r)
     int n2 =  r - m;
     int L[n1], R[n2];
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = array[l + i];
     for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
+        R[j] = array[m + 1+ j];
     i = 0;
     j = 0;
     k = l;
     while (i < n1 && j < n2){
         if (L[i] <= R[j]){
-            arr[k] = L[i];
+            array[k] = L[i];
             i++;
         }
         else{
-            arr[k] = R[j];
+            array[k] = R[j];
             j++;
         }
         k++;
     }
     while (i < n1){
-        arr[k] = L[i];
+        array[k] = L[i];
         i++;
         k++;
     }
     while (j < n2){
-        arr[k] = R[j];
+        array[k] = R[j];
         j++;
         k++;
     }
@@ -39,8 +39,8 @@ void merge(int array[], int l, int m, int r)
 void merge_sort(int array[], int l, int r){
     if (l < r){
         int m = l+(r-l)/2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-        merge(arr, l, m, r);
+        mergeSort(array, l, m);
+        mergeSort(array, m+1, r);
+        merge(array, l, m, r);
     }
 }
